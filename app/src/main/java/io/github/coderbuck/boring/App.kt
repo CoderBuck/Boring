7adf11c5c685929d767c53197b66b932d952143d
buck <coderbuck@163.com>
2020-04-07 15:02:25 +0800

    init App

---
+++ app/src/main/java/io/github/coderbuck/boring/App.kt
@@ -0,0 +1,14 @@
+package io.github.coderbuck.boring
+
+import android.app.Application
+import com.blankj.utilcode.util.Utils
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        Utils.init(this)
+    }
+}
