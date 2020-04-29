76d4b188ec20f7069cffbcb4c8a3cf39da582f58
buck <coderbuck@163.com>
2020-04-29 09:48:37 +0800

    clean

--- app/src/main/java/io/github/coderbuck/boring/api/Api.kt
+++ app/src/main/java/io/github/coderbuck/boring/api/Api.kt
@@ -1,9 +1,7 @@
 package io.github.coderbuck.boring.api
 
-import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object Api {
 
