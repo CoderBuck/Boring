7575509c4edd8879afdfc4c6185d5e45dcfcd4d2
buck <coderbuck@163.com>
2020-05-06 13:14:24 +0800

    知乎热榜抓取

---
+++ app/src/main/java/io/github/coderbuck/boring/bean/ZhihuHotItem.kt
@@ -0,0 +1,9 @@
+package io.github.coderbuck.boring.bean
+
+data class ZhihuHotItem(
+    val index: String,
+    val title: String,
+    val excerpt: String,
+    val metrics: String,
+    val img: String
+)
